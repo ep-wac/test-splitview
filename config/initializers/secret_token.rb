714dee97ea37964ed281cab09cc069e47554bf5d
Enrique Phillips <enrique.phillips.wac@gmail.com>
2011-05-18 22:25:58 +0200

    added Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSplitview::Application.config.secret_token = '3e6cb375814f315112af9867fc8ba0ebb0b91624f5d977ff54f892e6c21b6354b4a2c572ac06e8b99e04025e6e77cc8784e710c7eea5d522d36ae19134ac016e'
